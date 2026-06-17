8375151ae65c6479e61a3c852cde1eea46063e0f
P4D Midia <agenciap4d@gmail.com>
2026-06-17 11:46:17 -0300

    chore: optimize tailwind config and fix gitignore exclusions

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+    content: [
+        "./index.html",
+        "./App.tsx",
+        "./index.tsx",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./lib/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
